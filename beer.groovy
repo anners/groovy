1a91d0a2a041f412fd57d6f894897c91d9efbd50
awall6 <Ann.Wallace@nike.com>
2014-05-02 21:00:08 -0700

    print price

--- beer.groovy
+++ beer.groovy
@@ -3,17 +3,18 @@ println "Welcome to Anners Brew"
 println "On tap we have:"
 
 //list of beers
-def taplist = ["ipa", "lager", "esb", "dunkel", "stout"]
-for (i in taplist) { println i }
+def taplist = [ipa:5.00, lager:4.50, esb:5.50, dunkel:5.00, stout:4.75]
+taplist.each { k, v -> println "${k}" }
 
 def beer = System.console().readLine("What would you like to drink? ")
 
-if ( taplist.contains(beer) ) {
+if ( taplist.containsKey(beer) ) {
 //print price
 	println "Here is your $beer"
+	println ("You owe me $" +  taplist["${beer}"])
 } else {
 //make taplist print a method
-	println "Sorry we only don't have your beer"
+	println "Sorry we don't have your beer"
 } 
 
 
